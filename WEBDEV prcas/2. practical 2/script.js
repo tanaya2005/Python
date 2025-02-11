0c3fa6876eef7833843e721cdf956db09cdc114c
tanaya2005 <tanayaitis@gmail.com>
2025-02-11 16:48:28 +0530

    /

--- WEBDEV prcas/2. practical 2/script.js
+++
@@ -1,8 +0,0 @@
-function greetUser() {
-    const name = document.getElementById('name').value;
-    if (name) {
-      document.getElementById('output').innerText = `Hello, ${name}! Have a great day!`;
-    } else {
-      document.getElementById('output').innerText = 'Please enter your name.';
-    }
-  }
